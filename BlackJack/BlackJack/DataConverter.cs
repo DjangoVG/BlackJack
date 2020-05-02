00d02c7d49bbe400d3d9c0b1eda4722f8b63e965
DjangoVG <regis4d@hotmail.com>
2020-05-02 21:25:55 +0200

    :art: Ajout d'un DataConverter

---
+++ BlackJack/BlackJack/DataConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BlackJack
+{
+    public class DataConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Console.WriteLine("Convert");
+            return (value as string).ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Console.WriteLine("ConvertBack");
+            return (value as string).ToLower();
+        }
+    }
+}
--- BlackJack/BlackJack/FenetreOption.xaml.cs
+++ BlackJack/BlackJack/FenetreOption.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using BlackJackLibrairie;
+using System.Windows;
 using System.Windows.Media;
 
 namespace BlackJack
